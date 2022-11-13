cd05b66be240f8f2fd9058c159e7977cd889a5c0
Sejung Kwak <kwak.sejung@gmail.com>
2022-11-13 11:39:56 +0000

    mod: Customised admin panels for products app

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Product, Category
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
