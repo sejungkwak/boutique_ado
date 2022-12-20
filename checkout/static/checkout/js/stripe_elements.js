4e6083006762559bb0f24f0fd0e9b48fb8709eaa
Sejung Kwak <kwak.sejung@gmail.com>
2022-12-20 19:14:29 +0000

    feat: Add stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,24 @@
+const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+const client_secret = $('#id_client_secret').text().slice(1, -1);
+const stripe = Stripe(stripe_public_key);
+const elements = stripe.elements();
+const style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': {
+            color: '#fce883',
+        },
+        '::placeholder': {
+            color: '#aab7c4',
+        },
+    },
+    invalid: {
+        iconColor: '#dc3545',
+        color: '#dc3545',
+    },
+}
+const card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MEBNzHkm17RY51QGaCUWwIUqUfTG08RIx06TNXrEekadj0ohzjVvvwDzUPWllLVX7xt2dNegifdogObBHvOzoTt00IhslA65L',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
