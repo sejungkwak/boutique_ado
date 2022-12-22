06a0d3aa6697ca4ce4cc22abef3455f758d2b2e0
Sejung Kwak <kwak.sejung@gmail.com>
2022-12-22 20:58:05 +0000

    feat: Add loading overlay to checkout page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -46,6 +46,8 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     card.update({'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -60,6 +62,8 @@ form.addEventListener('submit', function(e) {
                 <span>${result.error.message}</span>
             `;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update('disabled', false);
             $('#submit-button').attr('disabled', false);
         } else {
