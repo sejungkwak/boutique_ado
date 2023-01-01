17e656f5a6c7ac7b6a85f2e37ee02490828e4d65
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-01 00:45:25 +0000

    feat: Create profiles app, add models, views and templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """
+    Display the user's profile
+    """
+    template = 'profile/profile.html'
+    context = {}
+
+    return render(request, template, context)
