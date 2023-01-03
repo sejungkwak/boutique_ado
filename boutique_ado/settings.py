a3c8bfd63c9d1a0b9bd148f96409adad79d16f96
Sejung Kwak <kwak.sejung@gmail.com>
2023-01-03 21:59:37 +0000

    fix: Fix database format

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -125,9 +125,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': {
-            'ENGINE': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-        }
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
